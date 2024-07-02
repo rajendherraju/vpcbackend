bf8d29718a300a7731da133fb6a910520f45278f
rajendherraju <38126617+rajendherraju@users.noreply.github.com>
2024-07-02 16:34:01 +0530

    Create security_groups.tf
    security

---
+++ security_groups.tf
@@ -0,0 +1,21 @@
+resource "aws_security_group" "jump_server_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["YOUR_IP_ADDRESS/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "jump-server-sg"
+  }
+}
